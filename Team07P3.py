6dc355561997be64c89e16940c541c8449055a23
gfaout <gautamf12@gmail.com>
2019-02-25 18:23:34 -0500

    Update Team07P3.py

--- Team07P3.py
+++ Team07P3.py
@@ -3,12 +3,21 @@
 #Project 3
 
 #Initialization and Dependency Imports
-
+##from __future__ import print_function
+##from __future__ import division
+import IMU
+import brickpi3
+import grovepi
+import time
 
 #IMU Setup
 try: 
 	imu = IMU.MPU9250()
 except: 
 	print("IMU Initialization Error")
+	
+#Brickpi Setup
+try:
+	bp = brickpi3.BrickPi3()
 	
 
