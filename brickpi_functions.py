3858d6ce580b450874b7f861a6a1fc3e51cf991e
gfaout <gautamf12@gmail.com>
2019-03-06 19:37:31 -0500

    Add files via upload
    This includes a function that can read the current gyro angle, a function that turns the motor of the ultrasonic to a certain point, and a function that currently sweeps all directions (but this will eventually integrate the ultrasonic read and output the path to take.

--- brickpi_functions.py
+++ brickpi_functions.py
@@ -1,8 +1,12 @@
+from __future__ import print_function
+from __future__ import division
+
 import brickpi3
 import time
 BP = brickpi3.BrickPi3()
 gyro_iterations = 5
-   
+delay = 0.01
+A = BP.PORT_A
 
 
 def get_curr_angle(EV3_GYRO):
@@ -16,5 +20,34 @@ def get_curr_angle(EV3_GYRO):
         angle[j] = angle[j] / gyro_iterations
     angle[0] = angle[0] % 360
     return angle
+
+def changePos(target):
+    try:
+        BP.offset_motor_encoder(A, BP.get_motor_encoder(A))
+    except IOError as error:
+        print(error)
+    current_pos = BP.get_motor_encoder(A)
+    while (current_pos != target):
+        direction = (target - current_pos) / abs(target - current_pos)
+        print(direction, target - current_pos)
+        if (abs(target - current_pos) < 5):
+            BP.set_motor_dps(A, -15*direction)
+            time.sleep(delay)
+        else:
+            BP.set_motor_dps(A,100*direction)
+            time.sleep(delay)
+        current_pos = BP.get_motor_encoder(A)
+    print("finished")
+    BP.set_motor_dps(A,0)
+    
+def checkThreeDirections():
+    changePos(90)
+    time.sleep(delay)
+    changePos(-90)
+    time.sleep(delay)
+    changePos(-90)
+    time.sleep(delay)
+    changePos(90)
+    time.sleep(0.02)
 
 
