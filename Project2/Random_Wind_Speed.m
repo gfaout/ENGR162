0cf4bb071893ac910c1555214f102b63fb2c7413
gfaout <gautamf12@gmail.com>
2019-04-02 18:04:11 -0400

    Add files via upload

---
+++ Project2/Random_Wind_Speed.m
@@ -0,0 +1,12 @@
+function va = Random_Wind_Speed(old_va, t_total)
+% outputs wind speed in m/s, dependent on the number of days passed.  
+if (mod((t_total * 2),2) == 0)
+shape_parameter = 2.1;
+scale_parameter = 10;
+wind_speed = wblrnd(scale_parameter, shape_parameter);
+wind_angle = unifrnd(0, 360);
+va = [wind_speed * cosd(wind_angle), wind_speed * sind(wind_angle)];
+elseif (mod((t_total * 2),2) == 1)
+va = old_va;
+end
+end
