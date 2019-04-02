ac625a6eccb5dd31a09939da3260e9cedd705b4a
Eric Zhu <ericzhu30@yahoo.com>
2019-04-01 21:42:10 -0400

    Update dragLoop.m

--- Project2/dragLoop.m
+++ Project2/dragLoop.m
@@ -7,7 +7,7 @@ currentTime = 0;
 airDensity = 1.225;
 waterDensity = 1025;
 %vAir = importdata('windData.csv');
-%vWater = importdata('currentData.csv');
+vWater = .11; %.11 m/s to .23 m/s Benguela Current
 %vIceberg = importdata('iceData.csv');
 %temp = importdata('temp.csv');
 iceWaterCd = 0.03;
@@ -19,17 +19,13 @@ length = 800; % Initial length
 height = 225; % Initial height
 
 while (currentTime < maxTime)
-    [height, length, width, volume_loss, mass_loss] = Iceberg_melt(vIceberg.data(currentTime), temp.data(currentTime), length, width, height, vAir.data(currentTime), vWater.data(currentTime), currentTime);
+    [height, length, width, volume_loss, mass_loss] = Iceberg_melt(vIceberg.data(currentTime), temp.data(currentTime), length, width, height, vAir.data(currentTime), vWater, currentTime);
 
-    draft = 2.91 * length^(0.71);
+    draft = height * 5 / 6;
     
-    if (draft > height) % Don't think this is necessary but just for precautions
-        draft = height;
-    end
-    
-    underFricDrag = dragForce(waterDensity, width * length, iceWaterCd,vWater.data(currentTime), vIceberg.data(currentTime)); %change parameters based on data
+    underFricDrag = dragForce(waterDensity, width * length, iceWaterCd,vWater, vIceberg.data(currentTime)); %change parameters based on data
     aboveFricDrag = dragForce(airDensity, width * length, iceAirCd,vAir.data(currentTime), vIceberg.data(currentTime));
-    underFormDrag = dragForce(waterDensity, draft * length, formCd,vWater.data(currentTime), vIceberg.data(currentTime));
-    aboveFormDrag = dragForce(airDensity, draft * length, formCd,vAir.data(currentTime), vIceberg.data(currentTime));
+    underFormDrag = dragForce(waterDensity, draft * length, formCd,vWater), vIceberg.data(currentTime));
+    aboveFormDrag = dragForce(airDensity, (height - draft) * length, formCd,vAir.data(currentTime), vIceberg.data(currentTime));
     currentTime = currentTime + dT;
 end
