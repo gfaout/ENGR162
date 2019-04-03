4967dce5e208c1fc6f0d2da53bc8a4e591cdd4f1
gfaout <gautamf12@gmail.com>
2019-04-02 20:01:27 -0400

    Add files via upload

--- Project2/Iceberg_Melt.m
+++ Project2/Iceberg_Melt.m
@@ -1,9 +1,9 @@
-function [new_height_iceberg, new_length_iceberg, new_width_iceberg, volume_loss, mass_loss] = Iceberg_Melt(vi, Tw, length_iceberg, width_iceberg,height_iceberg, va, vw, time_step)
+function [new_height_iceberg, new_length_iceberg, new_width_iceberg, volume_loss, mass_loss, height_loss] = Iceberg_Melt(vi, Tw, length_iceberg, width_iceberg,height_iceberg, va, vw, time_step)
 %Temperature of the iceberg assumed to be -10 degrees C
 %Time_step assumed to be 0.5 days, but is left variable.  
 
 %%Calculate Height and Side Loss%%
-height_loss = 0.58 * ((sqrt((vw(1) - vi(1))^2 + (vw(2) - vi(2))^2))^0.8) * (Tw - (-10)) / (length_iceberg ^ 0.2);
+height_loss = 0.58 * ((sqrt((vw(1) - vi(1))^2 + (vw(2) - vi(2))^2))^0.8) * (Tw - (-10)) * (abs(length_iceberg) ^ (-0.2));
 side_loss = 0.75 * (sqrt((va(1) - vw(1))^2 + (va(2) - vw(2))^2))^0.5 + 0.05 * sqrt((va(1)-vw(1))^2 + (va(2) - vw(2))^2) + 0.0076 * Tw + 0.0013 * (Tw)^2;
 
 %%Calculate New Dimensions%%
--- Project2/Water_Temperature.m
+++ Project2/Water_Temperature.m
@@ -1,6 +1,6 @@
 function temp_water = Water_Temperature(dist_traveled)
 if (dist_traveled < 2500)
-temp_water = 15 + (dist_traveled/2604) * 5;
+temp_water = 15 + (dist_traveled/2500) * 5;
 elseif (dist_traveled > 2500 && dist_traveled <= 2604)
 temp_water = 20 - (dist_traveled - 2500)/104 * 3;    
 end
