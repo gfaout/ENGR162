d3b0bebb815ef632abd39d547dc34e556a6eb113
Eric Zhu <ericzhu30@yahoo.com>
2019-04-02 20:03:07 -0400

    Update dragLoop.m

--- Project2/dragLoop.m
+++ Project2/dragLoop.m
@@ -9,7 +9,6 @@ waterDensity = 1025;
 %vAir = importdata('windData.csv');
 vWater = .11; %.11 m/s to .23 m/s Benguela Current
 vIceberg = 0.194444 %0.19444 m/s to 1 m/s natural movement
-%temp = importdata('temp.csv');
 iceWaterCd = 0.03;
 iceAirCd = 0.03 * 10^-3;
 formCd = 0.82;
@@ -19,13 +18,13 @@ length = input('Iceberg inital length: '); % Initial length 800
 height = input('Iceberg inital height: '); % Initial height 225
 
 while (currentTime < maxTime)
-    [height, length, width, volume_loss, mass_loss] = Iceberg_melt(vIceberg.data(currentTime), temp.data(currentTime), length, width, height, vAir.data(currentTime), vWater, currentTime);
+    [height, length, width, volume_loss, mass_loss] = Iceberg_melt(vIceberg.data(currentTime), Water_Temperature(currentTime * vIceberg), length, width, height, vAir.data(currentTime), vWater, currentTime);
 
     draft = height * 5 / 6;
     
-    underFricDrag = dragForce(waterDensity, width * length, iceWaterCd,vWater, vIceberg); %change parameters based on data
-    aboveFricDrag = dragForce(airDensity, width * length, iceAirCd,vAir.data(currentTime), vIceberg);
-    underFormDrag = dragForce(waterDensity, draft * length, formCd,vWater), vIceberg);
+    underFricDrag = dragForce(waterDensity, width * length, iceWaterCd, vWater, vIceberg); %change parameters based on data
+    aboveFricDrag = dragForce(airDensity, width * length, iceAirCd, vAir.data(currentTime), vIceberg);
+    underFormDrag = dragForce(waterDensity, draft * length, formCd, vWater, vIceberg);
     aboveFormDrag = dragForce(airDensity, (height - draft) * length, formCd,vAir.data(currentTime), vIceberg);
     currentTime = currentTime + dT;
 end
